ac351e6b73aa56cc530839fe3c0df191fb5eb737
Stephen Brennan <stephen@brennan.io>
2018-05-29 18:04:58 -0700

    Add sample script implementing a filter function

---
+++ scripts/filter.lisp
@@ -0,0 +1,10 @@
+(define filter (lambda (f l)
+    (if (null? l)
+      '()
+      (if (f (car l))
+        (cons (car l) (filter f (cdr l)))
+        (filter f (cdr l))))))
+
+(define main
+  (lambda (args)
+    (print (filter (lambda (x) (< x 5)) '(1 2 3 4 5 6 7 8 9 10)))))
