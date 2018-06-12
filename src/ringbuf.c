eacd5ddfc4312746bc69b1e1b3dcee8d61e760a8
Stephen Brennan <stephen@brennan.io>
2018-06-12 05:29:40 -0700

    Fix warnings in src/ringbuf.c

--- src/ringbuf.c
+++ src/ringbuf.c
@@ -19,56 +19,66 @@ void rb_destroy(struct ringbuf *rb)
 
 void rb_grow(struct ringbuf *rb)
 {
-	int i;
-	int oldalloc = rb->nalloc;
+	int i, oldalloc;
+	
+	oldalloc = rb->nalloc;
 	rb->nalloc *= 2;
 	rb->data = realloc(rb->data, rb->nalloc * rb->dsize);
 
 	for (i = 0; i < rb->count; i++) {
-		int oldindex = (rb->start + i) % oldalloc;
-		int newindex = (rb->start + i) % rb->nalloc;
+		int oldindex, newindex;
+		oldindex = (rb->start + i) % oldalloc;
+		newindex = (rb->start + i) % rb->nalloc;
 		if (oldindex != newindex) {
-			memcpy(rb->data + newindex * rb->dsize,
-			       rb->data + oldindex * rb->dsize, rb->nalloc);
+			memcpy((char*) rb->data + newindex * rb->dsize,
+			       (char*) rb->data + oldindex * rb->dsize,
+			       rb->nalloc);
 		}
 	}
 }
 
 void rb_push_front(struct ringbuf *rb, void *src)
 {
+	int newstart;
+
 	if (rb->count >= rb->nalloc) {
 		rb_grow(rb);
 	}
 
 	/* ensure the new start index is still positive */
-	int newstart = (rb->start + rb->nalloc - 1) % rb->nalloc;
+	newstart = (rb->start + rb->nalloc - 1) % rb->nalloc;
 	rb->start = newstart;
-	memcpy(rb->data + rb->start * rb->dsize, src, rb->dsize);
+	memcpy((char*)rb->data + rb->start * rb->dsize, src, rb->dsize);
 	rb->count++;
 }
 
 void rb_pop_front(struct ringbuf *rb, void *dst)
 {
-	int newstart = (rb->start + 1) % rb->nalloc;
-	memcpy(dst, rb->data + rb->start * rb->dsize, rb->dsize);
+	int newstart;
+	
+	newstart = (rb->start + 1) % rb->nalloc;
+	memcpy(dst, (char*)rb->data + rb->start * rb->dsize, rb->dsize);
 	rb->start = newstart;
 	rb->count--;
 }
 
 void rb_push_back(struct ringbuf *rb, void *src)
 {
+	int index;
 	if (rb->count >= rb->nalloc) {
 		rb_grow(rb);
 	}
 
-	int index = (rb->start + rb->count) % rb->nalloc;
-	memcpy(rb->data + index * rb->dsize, src, rb->dsize);
+	index = (rb->start + rb->count) % rb->nalloc;
+	memcpy((char*)rb->data + index * rb->dsize, src, rb->dsize);
 	rb->count++;
 }
 
 void rb_pop_back(struct ringbuf *rb, void *dst)
 {
-	int index = (rb->start + rb->count - 1) % rb->nalloc;
-	memcpy(dst, rb->data + index * rb->dsize, rb->dsize);
+	int index;
+
+	index = (rb->start + rb->count - 1) % rb->nalloc;
+	memcpy(dst, (char*)rb->data + index * rb->dsize, rb->dsize);
 	rb->count--;
 }
